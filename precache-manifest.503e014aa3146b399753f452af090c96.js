ab3be1c06dae196c22d7b2eb0cad80f91262b006
syamstechnology <51735129+syamstechnology@users.noreply.github.com>
2019-06-12 13:20:08 +0700

    First Deploy Diniyah React App

---
+++ precache-manifest.503e014aa3146b399753f452af090c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d35e39988e3a183bdc3a9d2570d20a9",
+    "url": "/static/media/NoDataFound.5d35e399.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cf104f69e8f91cebcc8",
+    "url": "/static/js/main.1cf104f6.chunk.js"
+  },
+  {
+    "revision": "1e8763a0ffdd32513dbe",
+    "url": "/static/js/1.1e8763a0.chunk.js"
+  },
+  {
+    "revision": "1cf104f69e8f91cebcc8",
+    "url": "/static/css/main.6acf228b.chunk.css"
+  },
+  {
+    "revision": "6313fd2a62dc961ffd081b419995e665",
+    "url": "/index.html"
+  }
+];
